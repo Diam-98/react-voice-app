729d1bb66c7a6bf8cc72b82471d574d3902734d5
Diam Diallo <70403651+Diam-98@users.noreply.github.com>
2023-07-06 18:28:09 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  const [text, setText] = useState("");
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [speech, setSpeech] = useState(null);
+
+  const handleTextReading = () => {
+    if (text) {
+      if (!isPlaying) {
+        const sp = new SpeechSynthesisUtterance(text);
+        [sp.voice] = speechSynthesis.getVoices();
+        speechSynthesis.speak(sp);
+        setSpeech(sp);
+        setIsPlaying(true);
+      } else {
+        stopSpeech();
+      }
+    } else {
+      alert("Veuillez entrez un texte d'abord");
+    }
+  };
+
+  const stopSpeech = () => {
+    if (speech) {
+      speechSynthesis.cancel();
+      setSpeech(null);
+      setIsPlaying(false);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <div className="box">
+        <h1>Un lecteur de text</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Vous entrer un texte dans ce champs puis vous cliquez sur lire pour
+          ecouter
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <textarea
+          placeholder="Ecrivez un texte..."
+          onChange={(event) => {
+            setText(event.target.value);
+          }}
+        />
+        <div className="button-and-voice">
+          {!isPlaying ? (
+            <button onClick={handleTextReading}>Lire</button>
+          ) : (
+            <button onClick={stopSpeech}>Arreter</button>
+          )}
+        </div>
+      </div>
     </div>
   );
 }
